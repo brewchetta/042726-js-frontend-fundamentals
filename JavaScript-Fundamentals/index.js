886be206f3f40d9ecc2c963552d84ca9712ce132
Chett <chett.tiller@gmail.com>
2026-04-27 12:52:40 -0400

    functionsssss

--- JavaScript-Fundamentals/index.js
+++ JavaScript-Fundamentals/index.js
@@ -48,6 +48,74 @@ if (5 > 6) {
 // DOM MANIPULATION
 // document object model
 
+// get an individual item
 const heading = document.querySelector("h1")
 
+// set attributes of an individual item
 heading.textContent = "Why Hello There"
+
+// get all items
+const allPTags = document.querySelectorAll("p")
+
+// get a specific item from all items
+const thirdPTag = allPTags[2]
+
+thirdPTag.style.fontFamily = "monospace"
+
+// JS STARTS COUNTING AT 0 SO [2] IS ACTUALLY THE THIRD ONE
+
+
+// ADDING ELEMENTS
+
+// 1. use document.createElement --> element type
+// makes the element but it is not actually on the page
+const welcomeMessage = document.createElement("h2") 
+
+// 2. change whatever is important about the element
+welcomeMessage.textContent = "Welcome to Lake Tahoe"
+
+// 3. give it a place to live (append or prepend)
+const body = document.body
+body.append(welcomeMessage)
+
+// REMOVING ELEMENTS
+
+// welcomeMessage.remove()
+
+// EXERCISE
+
+// // 1. Select the ul --> to be used later
+// const tahoeList = document.querySelector("ul")
+
+// // 2. Create a li
+// const newLi = document.createElement("li")
+
+// // 3. Change the li textContent so it reads a fun fact
+// newLi.textContent = "Lake Tahoe has trees"
+
+// // 4. Put the li somewhere so that it displays
+// tahoeList.append(newLi)
+
+
+// FUNCTIONS
+
+// D.R.Y. --> don't repeat yourself
+
+// newFact is an argument that will be given when we call the fn
+function addNewLakeTahoeFact( newFact ) {
+    // your instructions for the fn go here
+    const tahoeList = document.querySelector("ul")
+    const newLi = document.createElement("li")
+    newLi.textContent = newFact
+    tahoeList.append(newLi)
+}
+
+// invoke the fn (basically we make it happen)
+addNewLakeTahoeFact("Sasquatch lives in lake tahoe")
+
+// define a fn
+function addOne( num ) {
+    console.log(num + 1)
+}
+
+addOne(6)
