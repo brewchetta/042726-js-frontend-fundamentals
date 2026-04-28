20267edc98b4189052a832ff8358426dad03a52e
Chett <chett.tiller@gmail.com>
2026-04-28 16:06:32 -0400

    gsap simple tween

---
+++ GSAP-Intro/js/index.js
@@ -0,0 +1,5 @@
+        // element | duration | transformations
+gsap.from("#logo", { duration: 1, scale: 0, rotation:45, y: -300, x: -600 })
+// tween ^^^
+// a single small animation potentially part of a larger animation
+
