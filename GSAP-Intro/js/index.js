1c62f7c9db426e598a3e702624f0cad7b5884c30
Chett <chett.tiller@gmail.com>
2026-04-28 16:16:05 -0400

    easing

--- GSAP-Intro/js/index.js
+++ GSAP-Intro/js/index.js
@@ -1,5 +1,5 @@
-        // element | duration | transformations
-gsap.from("#logo", { duration: 1, scale: 0, rotation:45, y: -300, x: -600 })
+        // element | duration | ease/animation curve | transformations
+gsap.from("#logo", { duration: 1.5, ease: "bounce", scale: 0, rotation:45, y: -300 })
+
 // tween ^^^
 // a single small animation potentially part of a larger animation
-
