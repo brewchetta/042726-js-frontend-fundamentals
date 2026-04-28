7307877e3cc8a40d62c3920dfc2902894656d6a5
Chett <chett.tiller@gmail.com>
2026-04-28 17:05:55 -0400

    go mikey go

--- GSAP-Timelines/js/index.js
+++ GSAP-Timelines/js/index.js
@@ -1,6 +1,8 @@
-// michelangelo timeline initialization
-const mikeTimeline = gsap.timeline()
+// michelangelo timeline initialization | repeat is how many repeats after the first run
+const mikeTimeline = gsap.timeline({ repeat: 1, repeatDelay: 5 })
+// repeat -1 means the animation repeats FOREVER
 
-                // element | duration |  ease               | transformation
-mikeTimeline.from("#mike", { duration: 1, ease: "power1.in", y: -1000, delay: 1 })
-mikeTimeline.to("#mike", { duration: 1, ease: "power1.in", x: 1000, delay: 1 })
+mikeTimeline
+.from("#mike", { duration: 1, ease: "power1.in", y: -1000 })
+.to("#mike", { duration: 1, ease: "power1.in", x: 1000}, '+=0.5')
+// element | duration |  ease   |             transform | delay / timing
